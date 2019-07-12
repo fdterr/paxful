ec0f62da01fb60f4aa41cf30d85b155d220e850b
Rick Terry <fdterr@gmail.com>
2019-07-12 11:13:10 -0400

    secrets file now contains appropriate api keys and secrets

--- server/api/paxful.js
+++ server/api/paxful.js
@@ -1,13 +1,11 @@
+const axios = require('axios');
+const CryptoJS = require('crypto-js');
+
 const router = require('express').Router();
 module.exports = router;
 
-const accountSid = 'AC2008644491d7049070c862508688fdb8';
-const authToken = 'a6690f917c442db93543b1f6821b695e';
+const {apiKey, secret, accountSid, authToken} = require('../../secrets');
 const client = require('twilio')(accountSid, authToken);
-const XMLHttpRequest = require('xhr2');
-
-const axios = require('axios');
-const CryptoJS = require('crypto-js');
 
 router.get('/sendText', (req, res) => {
   try {
@@ -31,8 +29,6 @@ router.get('/', (req, res, next) => {
 
 router.get('/test', async (req, res, next) => {
   try {
-    const apiKey = 'YoMj3CtEAhokhLMVbnkAVGS31WJAxM3I';
-    const secret = '6jk9eNJU2WLJ53cMrQjcKjPl3eMqXUBk';
     // const offerType = 'sell';
     // const payment_method = 'paypal';
     const nonce = Date.now();
@@ -42,11 +38,6 @@ router.get('/test', async (req, res, next) => {
     const seal = CryptoJS.HmacSHA256(body, secret).toString();
     console.log('seal is', seal);
 
-    // const postBody = {
-    //   apikey: apiKey,
-    //   nonce: nonce,
-    //   seal: seal
-    // };
     const postBody = body + '&apiseal=' + seal;
     console.log('postBody is', postBody);
     const headers = {
@@ -54,27 +45,13 @@ router.get('/test', async (req, res, next) => {
       Accept: ' application/json; version=1'
     };
 
-    const {data} = await axios.post(
-      `https://paxful.com/api/offer/list`,
+    const response = await axios.post(
+      `https://paxful.com/api/trade/list`,
       postBody,
       {headers}
     );
-    console.log('data', data);
-    res.send(data);
-
-    // const xhr = new XMLHttpRequest();
-    // xhr.onreadystatechange = function() {
-    //   if (xhr.readyState == XMLHttpRequest.DONE) {
-    //     const response = JSON.parse(xhr.responseText);
-    //     console.log('response', response);
-    //     res.send(response);
-    //   }
-    // };
-
-    // xhr.open('POST', 'https://paxful.com/api/offer/list');
-    // xhr.setRequestHeader('Content-Type', 'text/plain');
-    // xhr.setRequestHeader('Accept', 'application/json');
-    // xhr.send(body + '&apiseal=' + seal);
+    console.log('headers', response.headers);
+    res.send(response.data);
   } catch (err) {
     next(err);
   }
