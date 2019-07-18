0b0b6e0588cba756d15fc36e1e7403119d11ca0f
Rick Terry <fdterr@gmail.com>
2019-07-18 12:17:53 -0400

    now downloading our trades to front-end

--- client/components/trades.js
+++ client/components/trades.js
@@ -1,9 +1,16 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import {Table} from 'semantic-ui-react';
+import {apiTrades} from '../store';
 
 class Trades extends Component {
   constructor() {
     super();
+  }
+
+  componentDidMount() {
+    console.log('props are', this.props);
+    this.props.getPaxfulTrades();
   }
 
   render() {
@@ -11,4 +18,17 @@ class Trades extends Component {
   }
 }
 
-export default Trades;
+const mapState = state => {
+  console.log('new state', state.paxful);
+  return {
+    trades: state.paxful.trades
+  };
+};
+
+const mapDispatch = dispatch => {
+  return {
+    getPaxfulTrades: () => dispatch(apiTrades())
+  };
+};
+
+export default connect(mapState, mapDispatch)(Trades);
---
+++ client/store/paxful.js
@@ -0,0 +1,69 @@
+import axios from 'axios';
+import history from '../history';
+
+/**
+ * ACTION TYPES
+ */
+const GET_TRADES = 'GET_TRADES';
+// const REMOVE_USER = 'REMOVE_USER';
+
+/**
+ * INITIAL STATE
+ */
+const defaultState = {};
+
+/**
+ * ACTION CREATORS
+ */
+const getTrades = trades => ({type: GET_TRADES, trades});
+// const removeUser = () => ({type: REMOVE_USER});
+
+/**
+ * THUNK CREATORS
+ */
+export const apiTrades = () => async dispatch => {
+  try {
+    const res = await axios.get('/api/paxful/trades');
+    dispatch(getTrades(res.data || defaultState));
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// export const auth = (email, password, method) => async dispatch => {
+//   let res;
+//   try {
+//     res = await axios.post(`/auth/${method}`, {email, password});
+//   } catch (authError) {
+//     return dispatch(getUser({error: authError}));
+//   }
+
+//   try {
+//     dispatch(getUser(res.data));
+//     history.push('/home');
+//   } catch (dispatchOrHistoryErr) {
+//     console.error(dispatchOrHistoryErr);
+//   }
+// };
+
+// export const logout = () => async dispatch => {
+//   try {
+//     await axios.post('/auth/logout');
+//     dispatch(removeUser());
+//     history.push('/login');
+//   } catch (err) {
+//     console.error(err);
+//   }
+// };
+
+/**
+ * REDUCER
+ */
+export default function(state = defaultState, action) {
+  switch (action.type) {
+    case GET_TRADES:
+      return action.trades;
+    default:
+      return state;
+  }
+}
