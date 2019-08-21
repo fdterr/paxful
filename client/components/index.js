8f2cde8e902a3554974ece0fa3fb90a242ef00ae
Rick Terry <fdterr@gmail.com>
2019-08-21 11:16:08 -0400

    capitalizstion...

--- client/components/index.js
+++ client/components/index.js
@@ -1,6 +1,6 @@
 export {default as Navbar} from './navbar';
 export {default as UserHome} from './user-home';
 export {Login, Signup} from './auth-form';
-export {default as Trades} from './Trades';
+export {default as Trades} from './trades';
 export {default as Offers} from './offers';
 export {default as Address} from './address';
